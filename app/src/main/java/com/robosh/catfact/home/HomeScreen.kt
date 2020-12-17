095df40b889b7eeb4cedcd4f6763604a2a97145e
Orest Shemeliuk <orestshemeliuk@gmail.com>
2020-12-17 19:37:25 +0200

    add binding and navigation to details screen

--- app/src/main/java/com/robosh/catfact/home/HomeScreen.kt
+++ app/src/main/java/com/robosh/catfact/home/HomeScreen.kt
@@ -5,15 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.robosh.catfact.R
+import com.robosh.catfact.databinding.FragmentHomeBinding
 
 class HomeScreen : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initClickListener()
+    }
+
+    private fun initClickListener() {
+        binding.catFactsButton.setOnClickListener {
+            findNavController().navigate(R.id.action_homeScreenFragment_to_detailsScreen)
+        }
     }
 }
