301da6f33006e3d45632c4ad482b1d96e51f872b
Orest Shemeliuk <orestshemeliuk@gmail.com>
2020-12-18 16:06:43 +0200

    refactor CatFactRepositotyImpl

--- app/src/main/java/com/robosh/catfact/net/repository/CatFactRepositoryImpl.kt
+++ app/src/main/java/com/robosh/catfact/net/repository/CatFactRepositoryImpl.kt
@@ -5,8 +5,8 @@ import androidx.lifecycle.LiveDataReactiveStreams
 import com.robosh.catfact.application.toObservableList
 import com.robosh.catfact.model.CatFact
 import com.robosh.catfact.model.ResultState
-import com.robosh.catfact.net.RetrofitImageClientInstance
 import com.robosh.catfact.net.RetrofitCatFactTextClientInstance
+import com.robosh.catfact.net.RetrofitImageClientInstance
 import com.robosh.catfact.net.api.CatFactApi
 import com.robosh.catfact.net.api.CatImageApi
 import io.reactivex.BackpressureStrategy
@@ -15,30 +15,32 @@ import io.reactivex.schedulers.Schedulers
 
 class CatFactRepositoryImpl : CatFactRepository {
 
+    private val catFactApi =
+        RetrofitCatFactTextClientInstance.retrofitInstance.create(CatFactApi::class.java)
+    private val catImageApi =
+        RetrofitImageClientInstance.retrofitInstance.create(CatImageApi::class.java)
+
     override fun getCatFacts(): LiveData<ResultState> {
-        val subscribe =
-            RetrofitCatFactTextClientInstance.retrofitInstance.create(CatFactApi::class.java).getCatFacts()
+        val observable =
+            catFactApi.getCatFacts()
                 .flatMap {
                     it.map { catFactResponse ->
-                        RetrofitImageClientInstance.retrofitInstance.create(CatImageApi::class.java)
-                            .getImageFile().map {
-                                Observable.just(CatFact(it.file, catFactResponse.text))
-                            }
-                            .onErrorReturn {
-                                Observable.just(CatFact("", catFactResponse.text))
-                            }
-                            .blockingFirst()
+                        catImageApi.getImageFile().map {
+                            Observable.just(CatFact(it.file, catFactResponse.text))
+                        }.onErrorReturn {
+                            Observable.just(CatFact("", catFactResponse.text))
+                        }.blockingFirst()
                     }.toObservableList()
                 }
                 .map {
                     val dataListState = ResultState.DataListState(it)
                     dataListState as ResultState
                 }
-                .onErrorReturn{
+                .onErrorReturn {
                     ResultState.ErrorState(it.localizedMessage ?: "Error in getCatFacts")
                 }
                 .subscribeOn(Schedulers.io())
 
-        return LiveDataReactiveStreams.fromPublisher(subscribe.toFlowable(BackpressureStrategy.DROP))
+        return LiveDataReactiveStreams.fromPublisher(observable.toFlowable(BackpressureStrategy.DROP))
     }
 }
